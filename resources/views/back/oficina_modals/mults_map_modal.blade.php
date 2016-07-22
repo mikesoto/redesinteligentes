592324a587758189f47a1689d41b022216f7535b
Mike Soto <desarrollowebuno@gmail.com>
2016-07-22 15:08:33 -0500

    make mults map modal large

--- resources/views/back/oficina_modals/mults_map_modal.blade.php
+++ resources/views/back/oficina_modals/mults_map_modal.blade.php
@@ -1,6 +1,6 @@
 <!-- Modal -->
 <div class="modal fade" id="mults-map-modal" tabindex="-1" role="dialog" aria-labelledby="mults-map-modal-label">
-  <div class="modal-dialog" role="document">
+  <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
